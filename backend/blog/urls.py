18f4bfc1d7cfc7b2f613544a91dedf0c0354eb2b
admin <admin@wp.pl>
2026-03-27 19:43:24 +0100

    fix Home page (stable version)

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,23 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include, re_path
 from rest_framework.routers import DefaultRouter
-from blog.views import PostViewSet
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from django.views.generic import TemplateView
+from .views import PostViewSet
 
 router = DefaultRouter()
-router.register(r'posts', PostViewSet)
+router.register(r'posts', PostViewSet, basename='post')
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-
-    # API endpoints
-    path('api/', include(router.urls)),
-
-    # JWT endpoints
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
-    # React catch-all route (wszystkie pozostałe ścieżki)
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
-]
+urlpatterns = router.urls
