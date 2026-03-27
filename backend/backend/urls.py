ffb36e9f95c7cb154b8d818554aa5c6092a65faf
admin <admin@wp.pl>
2026-03-27 20:02:27 +0100

    fix Home page (stable version)

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include, re_path
 from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('api/', include('blog.urls')),  # <- tylko /api/
     path('admin/', admin.site.urls),
-    path('api/', include('blog.urls')),  # wszystko co w blog.urls pod 'api/'
-    # Catch-all dla React
-    re_path(r'^.*$', TemplateView.as_view(template_name='../frontend/build/index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),  # React catch-all
 ]
