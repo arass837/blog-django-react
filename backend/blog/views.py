3dbe8ac5940a138bdbbd696ddc2c4c45c9075130
admin <admin@wp.pl>
2026-04-04 13:03:44 +0200

    fix api url

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from .models import Post
 from .serializers import PostSerializer
 
@@ -21,7 +21,8 @@ from .serializers import ViewsSerializer
 class ViewsViewSet(viewsets.ModelViewSet):
     queryset = Views.objects.all()
     serializer_class = ViewsSerializer
-    permission_classes = [IsAuthenticatedOrReadOnly]
+    # permission_classes = [IsAuthenticatedOrReadOnly]
+    permission_classes = [AllowAny]
 
     # 🔥 custom endpoint: /api/views/increment/
     @action(detail=False, methods=['post'])
